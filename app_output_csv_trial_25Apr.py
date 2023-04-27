430d6b4210b6d72ad2ff14bebc4dbb0661ba717c
lokiguna96 <131976636+lokiguna96@users.noreply.github.com>
2023-04-27 18:54:27 +0530

    Update app_output_csv_trial_25Apr.py

--- app_output_csv_trial_25Apr.py
+++ app_output_csv_trial_25Apr.py
@@ -6,7 +6,7 @@ import pandas as pd
 import asyncio
 from io import StringIO
 import csv
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Import modules needed for building the chatbot application
 from streamlit_chat import message
@@ -27,8 +27,8 @@ st.markdown(
     unsafe_allow_html=True)
 
 # Load API key from .env file
-load_dotenv()
-user_api_key = os.getenv("OPENAI_API_KEY")
+#load_dotenv()
+#user_api_key = os.getenv("OPENAI_API_KEY")
 user_api_key = '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'
 openai.api_key = user_api_key
 
